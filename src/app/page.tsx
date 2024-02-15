a26759706fc4ee006882f81d010e294e8247dbcf
Mateusz Minder <mateusz.minder99@gmail.com>
2024-02-15 23:21:56 +0100

    updating image width

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,31 @@
+'use client'
+
+import { Navbar } from "@/components/Navbar";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+
 export default function Home() {
+  const [viewportWidth, setViewPortWidth] = useState(0)
+
+  useEffect(() => {
+    const handleResize = () => {
+      setViewPortWidth(window.innerWidth)
+    };
+
+    handleResize();
+
+    window.addEventListener('resize', handleResize)
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+
   return (
-    <main className="w-full text-center">
+    <main className="flex-col w-full items-center justify-center text-center">
+      <Navbar />
+      <Image src="/placeholder-image-main.jpg" alt="Żołnierz w dziczy" width={viewportWidth} height={500} />
       <h1 className="text-2xl">SPS Iskra</h1>
     </main>
   );
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+export const Navbar = () => {
+  return (
+    <nav className="w-full flex items-center justify-center">
+      <div className="flex w-[500px] bg-slate-700 px-10 py-5 items-center justify-center text-white gap-3">
+        <h1 className="text-2x">Navbar</h1>
+        <span>Page 1</span>
+        <span>Page 2</span>
+        <span>Page 3</span>
+      </div>
+    </nav>
+  )
+}
