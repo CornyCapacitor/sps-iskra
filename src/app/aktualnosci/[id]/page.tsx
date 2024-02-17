3ab47323f19d31cab2cb42d18b3d6e23e64f8d57
Mateusz Minder <mateusz.minder99@gmail.com>
2024-02-17 23:49:06 +0100

    dynamic image inside dynamicaly generated page

--- src/app/aktualnosci/[id]/page.tsx
+++ src/app/aktualnosci/[id]/page.tsx
@@ -1,14 +1,20 @@
 'use client'
 
+import { PageImage } from "@/components/PageImage"
 import { useParams } from "next/navigation"
+import { useState } from "react"
 
 const Page = () => {
   const params = useParams()
+  const [image] = useState<string | null>(null)
 
   return (
-    <div>
-      <p>{params.id}</p>
-    </div>
+    <main className="flex-col w-full items-center justify-center text-center max-w-full overflow-x-hidden">
+      <PageImage imageUrl={`${image ? image : "/aktualnosci-placeholder.jpg"}`} />
+      <section className="flex flex-wrap gap-5 items-start justify-center p-10 min-h-[500px]">
+        <h1 className="text-3xl">{params.id}</h1>
+      </section>
+    </main>
   )
 }
 
