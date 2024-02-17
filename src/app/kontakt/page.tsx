a0687f9d01f0a94db9fe29a27468cd6322539fc4
Mateusz Minder <mateusz.minder99@gmail.com>
2024-02-17 16:32:36 +0100

    kontakt setup

--- src/app/kontakt/page.tsx
+++ src/app/kontakt/page.tsx
@@ -1,9 +1,13 @@
+import { PageImage } from "@/components/PageImage"
 
 const page = () => {
   return (
-    <div>
-      <p>Kontakt</p>
-    </div>
+    <main className="flex-col w-full items-center justify-center text-center max-w-full overflow-x-hidden">
+      <PageImage imageUrl={"/contact-placeholder.jpg"} />
+      <section className="flex flex-wrap gap-5 items-start justify-center p-10 min-h-[500px]">
+        <h1 className="text-3xl">Kontakt</h1>
+      </section>
+    </main>
   )
 }
 
