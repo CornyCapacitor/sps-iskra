6f0fd9b691e8c980e5473a044cbcfea9d4dc9808
Mateusz Minder <mateusz.minder99@gmail.com>
2024-02-17 23:58:09 +0100

    quick solution to go back to aktualnosci page

--- src/app/aktualnosci/[id]/page.tsx
+++ src/app/aktualnosci/[id]/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { PageImage } from "@/components/PageImage"
+import Link from "next/link"
 import { useParams } from "next/navigation"
 import { useState } from "react"
 
@@ -11,7 +12,8 @@ const Page = () => {
   return (
     <main className="flex-col w-full items-center justify-center text-center max-w-full overflow-x-hidden">
       <PageImage imageUrl={`${image ? image : "/aktualnosci-placeholder.jpg"}`} />
-      <section className="flex flex-wrap gap-5 items-start justify-center p-10 min-h-[500px]">
+      <section className="flex flex-col flex-wrap gap-5 items-center justify-start p-10 min-h-[500px]">
+        <Link className="w-full lg:w-[350px] p-3 rounded-md bg-gray-800 text-white hover:bg-gray-600 focus:outline-none text-center" href={"/aktualnosci"}>Wróć do aktualności</Link>
         <h1 className="text-3xl">{params.id}</h1>
       </section>
     </main>
