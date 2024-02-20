ea94144153564000fb1a7eb3cab82dbb165b5622
Mateusz Minder <mateusz.minder99@gmail.com>
2024-02-20 13:26:22 +0100

    admin workplace setup

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -7,9 +7,14 @@ import { useAtom } from "jotai"
 import { useRouter } from "next/navigation"
 
 import Link from "next/link"
+import { useState } from "react"
 
 const AdminPage = () => {
   const [user] = useAtom(spsIskraAuthAtom)
+
+  const [news, setNews] = useState<any>([])
+  const [competitions, setCompetitions] = useState<any>([])
+  const [trainings, setTrainings] = useState<any>([])
 
   const router = useRouter();
   const supabase = createClientComponentClient();
@@ -23,11 +28,47 @@ const AdminPage = () => {
     })
   }
 
+  const fetchNews = () => {
+    setNews([{ id: "dupa" }, { id: "kupa" }])
+  }
+
+  const fetchCompetitions = () => {
+    setCompetitions([{ id: "dupa" }, { id: "kupa" }])
+  }
+
+  const fetchTrainings = () => {
+    setTrainings([{ id: "dupa" }, { id: "kupa" }])
+  }
+
   if (user) {
     return (
       <main className="pt-[300px] min-h-screen flex items-start justify-center bg-gray-800 p-6 text-white text-center">
         <div className="bg-gray-900 p-8 rounded-lg shadow-md w-[95%] flex flex-col items-center justify-center gap-5">
-          <button className="w-[350px] p-3 rounded-md bg-blue-600 text-white hover:bg-blue-700 focus:outline-none text-center" onClick={() => handleLogout()}>Wyloguj</button>
+          <button className="w-[150px] p-3 rounded-md bg-gray-700 text-white hover:bg-gray-600 focus:outline-none text-center self-end" onClick={() => handleLogout()}>Wyloguj</button>
+          {news.length ?
+            <>
+              <button className="w-[350px] p-3 rounded-md bg-blue-600 text-white hover:bg-blue-700 focus:outline-none text-center" onClick={() => setNews([])}>Schowaj aktualności</button>
+              <span>Aktualności</span>
+            </>
+            :
+            <button className="w-[350px] p-3 rounded-md bg-blue-600 text-white hover:bg-blue-700 focus:outline-none text-center" onClick={() => fetchNews()}>Pokaż aktualności</button>
+          }
+          {competitions.length ?
+            <>
+              <button className="w-[350px] p-3 rounded-md bg-blue-600 text-white hover:bg-blue-700 focus:outline-none text-center" onClick={() => setCompetitions([])}>Schowaj zawody</button>
+              <span>Zawody</span>
+            </>
+            :
+            <button className="w-[350px] p-3 rounded-md bg-blue-600 text-white hover:bg-blue-700 focus:outline-none text-center" onClick={() => fetchCompetitions()}>Pokaż zawody</button>
+          }
+          {trainings.length ?
+            <>
+              <button className="w-[350px] p-3 rounded-md bg-blue-600 text-white hover:bg-blue-700 focus:outline-none text-center" onClick={() => setTrainings([])}>Schowaj zkolenia</button>
+              <span>Szkolenia</span>
+            </>
+            :
+            <button className="w-[350px] p-3 rounded-md bg-blue-600 text-white hover:bg-blue-700 focus:outline-none text-center" onClick={() => fetchTrainings()}>Pokaż szkolenia</button>
+          }
         </div>
       </main>
     )
