dfc7aff6e774d9ce6bad62f21c10b13b29e5648e
Mateusz Minder <mateusz.minder99@gmail.com>
2024-02-25 15:28:38 +0100

    fully functional news edit/delete/create without functional choose image function

--- src/app/admin/aktualnosci/[id]/page.tsx
+++ src/app/admin/aktualnosci/[id]/page.tsx
@@ -2,16 +2,18 @@
 
 import { spsIskraAuthAtom } from '@/state/atoms'
 import { useAtom } from 'jotai'
-import { useParams } from 'next/navigation'
+import { useParams, useRouter } from 'next/navigation'
 import { useEffect, useState } from 'react'
 
 import supabase from '@/app/config/supabaseClient'
 import Image from 'next/image'
 import Link from 'next/link'
+import Swal from 'sweetalert2'
 
 const Page = () => {
   const [user] = useAtom(spsIskraAuthAtom)
   const params = useParams()
+  const router = useRouter()
 
   const [data, setData] = useState<News>()
   const [title, setTitle] = useState("")
@@ -20,6 +22,9 @@ const Page = () => {
 
   const [tempImageUrl, setTempImageUrl] = useState<string | null>(null)
   const [newFile, setNewFile] = useState<File | null>(null)
+
+  const themeBackground = "#000000"
+  const themeColor = "#ffffff"
 
   const fetchData = async () => {
     const { data } = await supabase
@@ -39,8 +44,47 @@ const Page = () => {
     fetchData()
   }, [])
 
-  const saveChanges = () => {
-    return
+  const handleUpdateChanges = () => {
+    Swal.fire({
+      icon: 'question',
+      iconColor: '#2563eb',
+      background: `${themeBackground}`,
+      color: `${themeColor}`,
+      title: "Czy na pewno chcesz zaktualizować tę aktualność?",
+      showConfirmButton: true,
+      confirmButtonText: "Tak",
+      showCancelButton: true,
+      cancelButtonText: "Wróć",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        updateChanges()
+        router.push('/admin')
+      }
+      return
+    })
+  }
+
+  const updateChanges = async () => {
+    if (!user) {
+      return
+    }
+
+    const { data, error } = await supabase
+      .from('aktualnosci')
+      .update({
+        title: title,
+        description: description,
+      })
+      .eq('id', params.id)
+      .select()
+
+    if (data) {
+      return data
+    }
+
+    if (error) {
+      console.error(error)
+    }
   }
 
   const abortChanges = () => {
@@ -62,9 +106,42 @@ const Page = () => {
     reader.readAsDataURL(file)
   }
 
-  const deleteNews = () => {
+  const handleDeleteNews = () => {
+    Swal.fire({
+      icon: 'question',
+      iconColor: '#2563eb',
+      background: `${themeBackground}`,
+      color: `${themeColor}`,
+      title: "Czy na pewno chcesz usunąć tę aktualność?",
+      showConfirmButton: true,
+      confirmButtonText: "Tak",
+      showCancelButton: true,
+      cancelButtonText: "Wróć",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        deleteNews()
+        router.push('/admin')
+      }
+      return
+    })
+  }
+
+  const deleteNews = async () => {
     if (!user) {
       return
+    }
+
+    const { data, error } = await supabase
+      .from('aktualnosci')
+      .delete()
+      .eq('id', params.id)
+
+    if (data) {
+      return data
+    }
+
+    if (error) {
+      console.error(error)
     }
   }
 
@@ -77,12 +154,12 @@ const Page = () => {
           <>
             <input className="w-[350px] p-3 rounded-md border border-gray-700 bg-gray-800 text-white placeholder-gray-500 focus:outline-none focus:border-blue-500" value={title} onChange={(e) => setTitle(e.target.value)} placeholder="Tytuł aktualności" />
             <textarea className="w-[350px] min-h-[350px] p-3 rounded-md border border-gray-700 bg-gray-800 text-white placeholder-gray-500 focus:outline-none focus:border-blue-500 scrollbar_hidden" value={description} onChange={(e) => setDescription(e.target.value)} placeholder="Opis aktualności" />
-            <Image src={tempImageUrl || `/${imageUrl}`} alt="Zdjęcie aktualności" width={350} height={350} className="rounded-lg" />
+            <Image src={tempImageUrl || `/sps-iskra-logo.jpg`} alt="Zdjęcie aktualności" width={350} height={350} className="rounded-lg" />
             <p>Wybierz inne zdjęcie klikając poniżej:</p>
             <input type="file" className="w-[350px] flex items-center justify-center text-center" onChange={changeImage} />
-            <button className="w-[350px] p-3 rounded-md bg-green-600 text-white hover:bg-green-700 focus:outline-none text-center" onClick={() => saveChanges()}>Zapisz zmiany</button>
+            <button className="w-[350px] p-3 rounded-md bg-green-600 text-white hover:bg-green-700 focus:outline-none text-center" onClick={() => handleUpdateChanges()}>Zapisz zmiany</button>
             <button className="w-[350px] p-3 rounded-md bg-blue-600 text-white hover:bg-blue-700 focus:outline-none text-center" onClick={() => abortChanges()}>Odrzuć zmiany</button>
-            <button className="w-[350px] p-3 rounded-md bg-red-600 text-white hover:bg-red-700 focus:outline-none text-center" onClick={() => deleteNews()}>Usuń aktualność</button>
+            <button className="w-[350px] p-3 rounded-md bg-red-600 text-white hover:bg-red-700 focus:outline-none text-center" onClick={() => handleDeleteNews()}>Usuń aktualność</button>
           </>
         )}
       </div>
