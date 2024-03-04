e4bebcfc522f44d84d9858653658912e287df793
Mateusz Minder <mateusz.minder99@gmail.com>
2024-03-04 17:43:44 +0100

    historical aktualnosci page

--- src/app/aktualnosci/page.tsx
+++ src/app/aktualnosci/page.tsx
@@ -38,7 +38,7 @@ const NewsPage = () => {
     <main className="flex-col w-full items-center justify-center text-center max-w-full overflow-x-hidden">
       <PageImage imageUrl={"/news.jpg"} />
       <section className="flex flex-col gap-5 items-center justify-start p-10 min-h-[500px]">
-        <h1 className="text-3xl">Aktualności</h1>
+        <h1 className="text-3xl">Ostatnie aktualności</h1>
         <section className="flex flex-wrap gap-5 items-start justify-center p-10">
           {news.map((news) => (
             <AppCard key={news.id} {...news} type="aktualnosci" />
