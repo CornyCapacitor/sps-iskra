5013586977794d97e0accf71f3ce10465e3b0368
Mateusz Minder <mateusz.minder99@gmail.com>
2024-03-05 21:47:20 +0100

    added proper links for footer tags

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,13 +5,13 @@ export const Footer = () => {
   return (
     <div className="flex flex-col gap-5 items-center justify-center p-5 bg-black">
       <div className="flex gap-10 items-center justify-center">
-        <Link href="https://www.youtube.com/" target="_blank" className="bg-white p-2 rounded-full hover:scale-110 transition-[0.2s]">
+        <Link href="https://www.youtube.com/channel/UC6GsLGbDqcQN4lAB4dmP3Qg" target="_blank" className="bg-white p-2 rounded-full hover:scale-110 transition-[0.2s]">
           <Image src="/youtube-icon.svg" alt="Youtube icon" width={75} height={75} />
         </Link>
         <Link href="https://www.facebook.com/profile.php?id=61551801553096" target="_blank" className="bg-white p-2 rounded-full hover:scale-110 transition-[0.2s]">
           <Image src="/facebook-icon.svg" alt="Facebook icon" width={75} height={75} />
         </Link>
-        <Link href="https://www.instagram.com/" target="_blank" className="bg-white p-2 rounded-full hover:scale-110 transition-[0.2s]">
+        <Link href="https://www.instagram.com/sps_iskra/" target="_blank" className="bg-white p-2 rounded-full hover:scale-110 transition-[0.2s]">
           <Image src="/instagram-icon.svg" alt="Instagram icon" width={75} height={75} />
         </Link>
       </div>
